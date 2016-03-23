2a7bfa930bc21108adcea8ec9a2092964fc4f111
Sam Zhou <zsy_iamtzzsy@163.com>
2016-03-23 00:09:47 -0700

    get start with spring boot

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,15 @@
 package app;
 
-/**
- * Created by szlizarrrr on 3/22/16.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("meetup")
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 }
