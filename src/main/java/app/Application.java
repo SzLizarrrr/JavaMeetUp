8e02d663fc1dc8892dd0a03b5f65c97361e81083
Sam Zhou <zsy_iamtzzsy@163.com>
2016-03-23 00:01:41 -0700

    first commit

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,7 @@
+package app;
+
+/**
+ * Created by szlizarrrr on 3/22/16.
+ */
+public class Application {
+}
---
+++ src/main/java/meetup/controller/GreetingController.java
@@ -0,0 +1,7 @@
+package meetup.controller;
+
+/**
+ * Created by szlizarrrr on 3/22/16.
+ */
+public class GreetingController {
+}
