d9433a81d2b446a68f1024a4d45e6d0d0fef3c82
Sam Zhou <zsy_iamtzzsy@163.com>
2016-03-28 22:03:15 -0700

    configuration

---
+++ src/main/java/meetup/config/DefaultPageConfig.java
@@ -0,0 +1,21 @@
+package meetup.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by ZHOUS024 on 3/28/2016.
+ */
+
+@Configuration
+public class DefaultPageConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/greeting");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+}
