acb5af8e4659648d80755b8dd7ae6e8e355c2f74
SzLizarrr <yf8189@gmail.com>
2017-06-08 16:01:05 -0700

    multiple properties manage the project in different env

--- src/test/java/meetup/controller/PageControllerTest.java
+++ src/test/java/meetup/controller/PageControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
